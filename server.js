4433992557b2ef68419da546440e1abc70950e35
pkanyu <petersonkariuki91@gmail.com>
2024-06-17 12:09:54 +0300

    served static files

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 module.exports = app;
+app.use(express.static('./'));
 
 /* Do not change the following line! It is required for testing and allowing
 *  the frontend application to interact as planned with the api server
